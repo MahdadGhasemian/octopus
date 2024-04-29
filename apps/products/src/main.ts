3b553a6308a91747a6ed2049d43405c46352523b
Mahdad Ghasemian <mahdad.ghasemian@gmail.com>
2024-04-29 09:28:32 +0330

    added swagger to products

--- apps/products/src/main.ts
+++ apps/products/src/main.ts
@@ -6,10 +6,24 @@ import * as cookieParser from 'cookie-parser';
 import { ValidationPipe } from '@nestjs/common';
 import { Logger } from 'nestjs-pino';
 import { KAFKA_PRODUCTS_NAME } from '@app/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(ProductsModule);
   const configService = app.get(ConfigService);
+  const documentOptions = new DocumentBuilder()
+    .setTitle('Products App')
+    .setDescription('Product Manager')
+    .setVersion('1.0')
+    .addServer(
+      `http://localhost:${configService.getOrThrow<string>('HTTP_PORT')}`,
+      'Local environment',
+    )
+    .addTag('Health')
+    .addTag('Categories')
+    .addTag('Products')
+    .build();
+
   app.connectMicroservice({
     transport: Transport.KAFKA,
     options: {
@@ -24,6 +38,9 @@ async function bootstrap() {
   app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.useLogger(app.get(Logger));
+
+  const document = SwaggerModule.createDocument(app, documentOptions);
+  SwaggerModule.setup('docs', app, document);
   await app.startAllMicroservices();
   await app.listen(configService.get('HTTP_PORT'));
 }
