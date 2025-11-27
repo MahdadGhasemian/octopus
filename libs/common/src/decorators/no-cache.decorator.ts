013a01904caa6b92d8d6287173c3150d27e6679b
Atishay Jain <atishay.jain@awiros.com>
2025-11-27 23:10:11 +0530

    Fix typo in ForceToClearCache decorator and update README

--- libs/common/src/decorators/no-cache.decorator.ts
+++ libs/common/src/decorators/no-cache.decorator.ts
@@ -6,5 +6,5 @@ export const FORCE_TO_CLEAR_CACHE_KEY = 'forceToClearCache';
 
 export const NoCache = () => SetMetadata(NO_CACHE_KEY, true);
 export const GeneralCache = () => SetMetadata(GENERAL_CACHE_KEY, true);
-export const FoceToClearCache = (key) =>
+export const ForceToClearCache = (key) =>
   SetMetadata(FORCE_TO_CLEAR_CACHE_KEY, key);
